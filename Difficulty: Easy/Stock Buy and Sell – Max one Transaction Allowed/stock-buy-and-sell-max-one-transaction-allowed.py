b54066c84173311194a660cc52bf2bcc7cc6b00e
Parv Jain <parvjainbpl@outlook.com>
2026-08-22 11:15:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Stock Buy and Sell – Max one Transaction Allowed/stock-buy-and-sell-max-one-transaction-allowed.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices):
+        # code here
+        max_profit = 0 
+        min_price = prices[0]
+        n = len(prices)
+        
+        for i in range (1,n):
+            
+            min_price= min( min_price, prices[i] )
+            
+            max_profit = max( max_profit, prices[i]-min_price )
+            
+        return max_profit
+        
